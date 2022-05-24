176fa9f2ac4e4c4dde77c77fd1e2033beefe3823
AlexCristian11 <alexpopescu2002@yahoo.com>
2022-05-24 15:50:14 +0300

    Added the dark mode functionality

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,33 +3,37 @@ import styled from 'styled-components';
 import image from '../img-src/placeholder.jpg';
 
 
-function About() {
+function About(props) {
   return (
     <AboutStyle id="about-me">
-        <div className="description">
-            <h2>About Me</h2>
-            <p>Hi! My name is Alex , a 19 yrs old student from Romania!</p>
-            <p>I study at Romanian-American University in Bucharest , School of Computer Science for Business Management. 
-            I started learning web development about 9 months ago and I loved it ever since. 
-            I started not knowing anything and now I am getting closer and closer to learning React! 
-            I really like making designs and starting to make them into a real website. 
-            I would like to expand my knowledge of web development and to work in a company with real challenges to solve.</p>
-        </div>
-        <img src={image} alt="" /> 
+        <div className={props.darkMode ? "dark" : ""} id="about-me-container">
+            <div className="description">
+                <h2>About Me</h2>
+                <p>Hi! My name is Alex , a 19 yrs old student from Romania!</p>
+                <p>I study at Romanian-American University in Bucharest , School of Computer Science for Business Management. 
+                I started learning web development about 9 months ago and I loved it ever since. 
+                I started not knowing anything and now I am getting closer and closer to learning React! 
+                I really like making designs and starting to make them into a real website. 
+                I would like to expand my knowledge of web development and to work in a company with real challenges to solve.</p>
+            </div>
+            <img src={image} alt="" />
+        </div> 
     </AboutStyle>
   )
 }
 
 const AboutStyle = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    width: 90%;
-    margin: 0 auto;
-    justify-content: space-around;
-    margin-bottom: 200px;
-    margin-top: 150px;
+    
+    #about-me-container {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        margin: 0 auto;
+        justify-content: space-around;
+    }
 
     .description {
+        margin-top: 150px;
         width: 60%;
         line-height: 1cm;
         display: flex;
@@ -50,6 +54,14 @@ const AboutStyle = styled.div`
         width: 350px;
         height: 350px;
         border-radius: 5%;
+        margin-top: 150px;
+    }
+
+    ${'' /* Dark mode */}
+
+    .dark {
+        background-color: #000;
+        color: #fff;
     }
 `
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,34 +2,36 @@ import React from 'react'
 import styled from 'styled-components';
 import { FaHtml5, FaCss3, FaGithub, FaChrome, FaTerminal, FaFigma, FaUniversalAccess, FaBootstrap, FaDatabase, FaReact} from 'react-icons/fa';
 import { SiJavascript } from 'react-icons/si';
-function Skills() {
+function Skills(props) {
   return (
     <Icons id="skills">
-        <h2>Skills</h2>
-        <div className="skills">
-            <FaHtml5 className="icon" size="50px"/>
-            <FaCss3 className="icon" size="50px"/>
-            <SiJavascript className="icon" size="50px"/>
-            <FaGithub className="icon" size="50px"/>
-            <FaChrome className="icon" size="50px"/>
-            <FaTerminal className="icon" size="50px"/>
-            <FaFigma className="icon" size="50px"/>
-            <FaUniversalAccess className="icon" size="50px"/>
-            <FaBootstrap className="icon" size="50px"/>
-            <FaDatabase className="icon" size="50px"/>
-            <FaReact className="icon" size="50px"/>
+        <div className={props.darkMode ? "dark" : ""}>
+            <h2>Skills</h2>
+            <div className="skills">
+                <FaHtml5 className="icon" size="50px"/>
+                <FaCss3 className="icon" size="50px"/>
+                <SiJavascript className="icon" size="50px"/>
+                <FaGithub className="icon" size="50px"/>
+                <FaChrome className="icon" size="50px"/>
+                <FaTerminal className="icon" size="50px"/>
+                <FaFigma className="icon" size="50px"/>
+                <FaUniversalAccess className="icon" size="50px"/>
+                <FaBootstrap className="icon" size="50px"/>
+                <FaDatabase className="icon" size="50px"/>
+                <FaReact className="icon" size="50px"/>
+            </div>
         </div>
     </Icons>
   )
 }
 
 const Icons = styled.div`
-    margin: 80px auto;
 
     h2 {
         text-align: center;
         margin-bottom: 50px;
         font-size: 1.75rem;
+        padding-top: 50px;
     }
 
     .skills {
@@ -38,6 +40,18 @@ const Icons = styled.div`
         display: flex;
         justify-content: space-around;
         flex-wrap: wrap;
+        padding: 30px 0 70px 0;
+    }
+
+    ${'' /* Dark mode */}
+
+    .dark {
+        background-color: #000;
+        color: #fff;
+    }
+
+    .dark .icon {
+        color: #fff;
     }
 
 `
